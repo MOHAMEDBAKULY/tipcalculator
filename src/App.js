d551895add531248a0e829c47389eac7506af564
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-06-15 08:34:20 +0300

    Understand your code

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function Calculator() {
   const [percentage1, setPercentage1] = useState(0);
   const [percentage2, setPercentage2] = useState(0);
 
-  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+  const tip = (bill * ((percentage1 + percentage2) / 2)) / 100;
 
   function handleReset() {
     setBill("");
